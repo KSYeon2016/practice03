e9198c7dc84628616e4dc604fab1d8726ac81ec0
S401 <S401@S401-08>
2016-07-11 15:44:47 +0900

    prob2 수정

--- src/prob2/Prob2.java
+++ src/prob2/Prob2.java
@@ -7,16 +7,17 @@ public class Prob2 {
 		Scanner scanner = new Scanner(System.in);
 		int[] bill = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 5, 1};
 		int len = bill.length;
-		int[] n = new int[len];
 		
 		System.out.print("금액 : ");
 		int money = scanner.nextInt();
 		
 		for(int i = 0; i < len; i++){
-			n[i] = money / bill[i];			// 몫
-			money = money - bill[i]*n[i];	// 잔액
-			System.out.println(bill[i] + "원 : " + n[i] + "개");
+			int n = money / bill[i];	// 몫
+			money = money - bill[i]*n;	// 잔액
+			System.out.println(bill[i] + "원 : " + n + "개");
 		}
+		
+		scanner.close();
 	}
 }
 
